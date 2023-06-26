c432cd45365e7d525375e8eccff6c5e05a120337
Demis Bellot <demis.bellot@gmail.com>
2023-06-27 02:13:26 +0800

    Update Configure.AppHost.cs

--- Gallery.Server/Configure.AppHost.cs
+++ Gallery.Server/Configure.AppHost.cs
@@ -22,7 +22,6 @@ public class AppHost : AppHostBase, IHostingStartup
             allowOriginWhitelist: new[]{
             "http://localhost:5000",
             "https://localhost:5001",
-            "https://localhost:5002",
             "https://localhost:7142",
             "http://localhost:3000",
             "http://localhost:5173",
