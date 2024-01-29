4a61d85362adfe8a08df85d89a52d6901dd043e5
Demis Bellot <demis.bellot@gmail.com>
2024-01-29 14:58:19 +0800

    Restore missing AppData populated in ScriptContext

--- MyApp/Configure.AppHost.cs
+++ MyApp/Configure.AppHost.cs
@@ -1,11 +1,12 @@
 ﻿using Funq;
 using MyApp.ServiceInterface;
+using MyApp.ServiceModel;
 
 [assembly: HostingStartup(typeof(MyApp.AppHost))]
 
 namespace MyApp;
 
-public class AppHost : AppHostBase, IHostingStartup
+public class AppHost() : AppHostBase("MyApp", typeof(MyServices).Assembly), IHostingStartup
 {
     public void Configure(IWebHostBuilder builder) => builder
         .ConfigureServices((context, services) =>
@@ -13,12 +14,29 @@ public class AppHost : AppHostBase, IHostingStartup
             // Configure ASP.NET Core IOC Dependencies
         });
 
-    public AppHost() : base("MyApp", typeof(MyServices).Assembly) { }
-
     // Configure your AppHost with the necessary configuration and dependencies your App needs
     public override void Configure(Container container)
     {
-        SetConfig(new HostConfig {
+        SetConfig(new HostConfig
+        {
         });
+
+        ScriptContext.Args[nameof(AppData)] = new AppData
+        {
+            Currencies = NumberCurrency.All,
+            AlphaValues = ["Alpha", "Bravo", "Charlie"],
+            AlphaDictionary = new()
+            {
+                ["A"] = "Alpha",
+                ["B"] = "Bravo",
+                ["C"] = "Charlie",
+            },
+            AlphaKeyValuePairs = new()
+            {
+                new("A", "Alpha"),
+                new("B", "Bravo"),
+                new("C", "Charlie"),
+            },
+        };
     }
 }
